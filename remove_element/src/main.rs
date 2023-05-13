8a55d999d3ee3cebfb20721c8c6ae9acefa5f3b7
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-05-14 01:02:00 +0800

    code

--- remove_element/src/main.rs
+++ remove_element/src/main.rs
@@ -1,1 +1,15 @@
 fn main() {}
+
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut k = 0;
+    if nums.len() == 0 {
+        return k as i32;
+    }
+    for i in 0..nums.len() {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k += 1;
+        }
+    }
+    k as i32
+}
